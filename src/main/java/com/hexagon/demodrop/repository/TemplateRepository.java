eba9d5902b45becb560b571b9c992511a89f006a
Robbie Keeris <robbie.keeris@gmail.com>
2019-12-10 13:29:17 +0100

    Added Security Configuration for UserDetailService

---
+++ src/main/java/com/hexagon/demodrop/repository/TemplateRepository.java
@@ -0,0 +1,8 @@
+package com.hexagon.demodrop.repository;
+
+import com.hexagon.demodrop.model.Template;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TemplateRepository extends CrudRepository<Template,Long> {
+}
+
