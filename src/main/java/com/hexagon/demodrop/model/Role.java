a3d9714a1c9f04c02f261a58479d3d514fa4bb77
Robbie Keeris <robbie.keeris@gmail.com>
2019-12-10 09:16:11 +0100

    Added Security Configuration for UserDetailService

--- src/main/java/com/hexagon/demodrop/model/Role.java
+++ src/main/java/com/hexagon/demodrop/model/Role.java
@@ -1,4 +1,4 @@
-package model;
+package com.hexagon.demodrop.model;
 
 import javax.persistence.*;
 import java.util.Collection;
@@ -7,7 +7,7 @@ import java.util.Collection;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
