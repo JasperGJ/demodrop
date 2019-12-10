b38fbf13f200ed23a7a1987725fb63500e12928c
JasperGJ <jasperstel@gmail.com>
2019-12-10 12:49:48 +0100

    Added Token class

---
+++ src/main/java/com/hexagon/demodrop/model/Token.java
@@ -0,0 +1,53 @@
+package com.hexagon.demodrop.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private long user_id;
+    private Date date;
+
+    public Token() {
+    }
+
+    public Token(long user_id) {
+        this.user_id = user_id;
+        this.date = new Date();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
+
+
+
+
