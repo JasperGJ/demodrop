ffecdb9929da4708268af3d4f22adf3c77540ec7
JasperGJ <jasperstel@gmail.com>
2019-12-11 15:08:37 +0100

    Added ResetPassword method

--- src/main/java/com/hexagon/demodrop/service/UserService.java
+++ src/main/java/com/hexagon/demodrop/service/UserService.java
@@ -1,7 +1,9 @@
 package com.hexagon.demodrop.service;
 
 import com.hexagon.demodrop.model.User;
+import com.hexagon.demodrop.model.Token;
 import com.hexagon.demodrop.object.LoginData;
+import com.hexagon.demodrop.repository.TokenRepository;
 import com.hexagon.demodrop.repository.UserRepository;
 import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,8 @@ import java.io.IOException;
 public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
+    private TokenRepository tokenRepository;
+    private EmailService emailService;
 
     @Autowired
     public UserService(UserRepository userRepository) {
@@ -55,17 +59,43 @@ public class UserService implements UserDetailsService {
         return true;
     }
 
-    private ByteArrayOutputStream createThumbnail(MultipartFile orginalFile, Integer width) throws IOException {
+    private ByteArrayOutputStream createThumbnail(MultipartFile originalFile, Integer width) throws IOException {
         ByteArrayOutputStream thumbOutput = new ByteArrayOutputStream();
         BufferedImage thumbImg = null;
-        BufferedImage img = ImageIO.read(orginalFile.getInputStream());
+        BufferedImage img = ImageIO.read(originalFile.getInputStream());
         thumbImg = Scalr.resize(img, Scalr.Method.AUTOMATIC, Scalr.Mode.AUTOMATIC, width, Scalr.OP_ANTIALIAS);
-        ImageIO.write(thumbImg, orginalFile.getContentType().split("/")[1] , thumbOutput);
+        ImageIO.write(thumbImg, originalFile.getContentType().split("/")[1] , thumbOutput);
         return thumbOutput;
     }
 
-    //TODO  method resetpassword
-    // parameter email
+    public boolean ResetPassword(String email) {
+        User user = userRepository.findByEmail(email);
+        if (user == null) throw new UsernameNotFoundException(String.format("Username[%s] not found", email));
+
+        Token token = (new Token(user.getId()));
+        tokenRepository.save(token);
+
+        emailService.sendMail(
+                "Password Reset",
+                "Hexagonian,\n You've requested a password reset.\n Click the link below to reset your password.\n http://localhost:8080/confirmreset?token=" + token.getId() + "\nIf you didn't request a reset you can ignore this email.",
+                user.getEmail());
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //TODO method confirmreset
     // paraneter token
