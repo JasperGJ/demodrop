961936dae8db99b52762173087e81b9f3d4cbca9
Robbie Keeris <robbie.keeris@gmail.com>
2019-12-12 09:40:15 +0100

    Added Form and Fetch

--- src/reactjs/src/EditProfile/EditProfile.js
+++ src/reactjs/src/EditProfile/EditProfile.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, {useRef,useState,useEffect}  from 'react';
 import foto from '../dondiablo-portret.jpg';
 import './EditProfile.css';
 import InputText from '../Components/InputText/InputText';
 import Button from "../Components/Button/Button";
 
-const editProfile = () => {
-        return (
+function EditProfile(){
+    const photoData = useRef(null);
+    const profileForm = useRef(null);
+    const [files,setFiles] = useState([]);
+
+    const doProfile = (event) => {
+        event.preventDefault();
+        const profile = new FormData(profileForm.current);
+        profile.append("photo",files[0]);
+        fetch(profileForm.current.action, {
+            method: profileForm.current.method,
+            body: profile
+        })
+            .then(response => {
+                console.log("getProfile", response);
+                const contentType = response.headers.get('content-type');
+                if (contentType && contentType.includes('application/json'))
+                    return response.json();
+                return response.text();
+            })
+            .then(json => {} )
+            .catch(e => console.warn(e));
+
+    };
+
+
+    function onPhotoChoosen() {
+        setFiles([...photoData.current.files]);
+    }
+
+    return (
         <div>
             <h2>EDIT PROFILE</h2>
-            <img className="EditProfilePhoto" src={foto} alt="foto" />
-            <InputText  placeholder="ARTIST NAME"/>
-            <br/>
-            <textarea placeholder="Type your BIO"/>
-            <br/>
-            <Button type="submit">SAVE</Button>
+            <div>
+                <h1>Edit Profile</h1>
+                <div>
+                    {files.length > 0 ? <img style={{
+                        margin: "10px",
+                        width: "200px",
+                        height: "200px"
+                    }} src={URL.createObjectURL(files[0])} alt="preview profile"/> : ""}
+                </div>
+                <form method="post" action="/profile"onSubmit={doProfile} ref={profileForm}>
+                    <p><input type="text" name="name" placeholder="enter name"/></p>
+                    <p><input type="text" name="description" placeholder="enter description"/></p>
+                    <p><input type="file" name="photo" onChange={onPhotoChoosen} ref={photoData}/></p>
+                    <p><button type="submit">Update Profile</button></p>
+                </form>
+            </div>
         </div>
     )
 };
 
-export default editProfile;
+export default EditProfile;
