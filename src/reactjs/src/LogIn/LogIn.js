47b411d36220bde96360a448d8ab0bad59999c4c
Robbie Keeris <robbie.keeris@gmail.com>
2019-12-12 08:42:26 +0100

    Added Fetch

--- src/reactjs/src/LogIn/LogIn.js
+++ src/reactjs/src/LogIn/LogIn.js
@@ -1,63 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import AuthenticationService from '../AuthenticationService/AuthenticationService.js';
 import './LogIn.css';
 
+    class Login extends Component {
 
-// const logIn = (props) => {
-class Login extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            hasLoginFailed: false,
-            showSuccesMessage: false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.loginClicked = this.loginClicked.bind(this);
-    }
-
-
-    handleChange(event) {
-        console.log(this.state);
-        this.setState(
-            {
-                [event.target.name]: event.target.value
-            }
-        )
-    }
+        constructor(props) {
+            super(props);
+            this.state = {
+                username: "",
+                password: "",
+                hasLoginFailed: false,
+                showSuccesMessage: false
+            };
+            this.handleChange = this.handleChange.bind(this);
+            this.loginClicked = this.loginClicked.bind(this);
+        }
+    
+    
+        handleChange(event) {
+            console.log(this.state);
+            this.setState(
+                {
+                    [event.target.name]
+                        : event.target.value
+                }
+            )
+        }
 
     loginClicked() {
-        if (this.state.username === "hexagon" && this.state.password === "hexagon") {
-            AuthenticationService.registerSuccesfulLogin(this.state.username, this.state.password);
-            this.props.history.push(`/profile/${this.state.username}`);
-            this.setState({showSuccesMessage: true});
-            this.setState({hasLoginFailed: false})
-        } else {
-            this.setState({hasLoginFailed: true});
-            this.setState({showSuccesMessage: false});
-        }
-    }
+        const data = new FormData();
+        data.append("username",this.state.username);
+        data.append("password",this.state.password);
 
+        fetch("/login", {
+            method: "POST",
+            body: data
+        }).then(response => {
+                if (response.status === 200){
+                    return response.json();
+                }
+            this.setState({ hasLoginFailed: true });
+            this.setState({ showSuccesMessage: false });
+         })
+            .then(json => {
+                AuthenticationService.registerSuccesfulLogin(this.state.username, this.state.password);
+                this.props.history.push(`${json.role}`);
+                this.setState({ showSuccesMessage: true });
+                this.setState({ hasLoginFailed: false })
+            })
+            .catch(e => console.warn(e))
+        }
+    
     render() {
-        return (
-            <div>
-                <h2 className="LoginH2">LOGIN</h2>
-                {this.state.hasLoginFailed && <div>invalid credentials</div>}
-                {this.state.showSuccesMessage && <div>login succesfull </div>}
-                <div><input placeholder="USERNAME" type="text" name="username" value={this.state.username}
-                            onChange={this.handleChange}/>
-                    <br/>
-                    <input placeholder="PASSWORD" type="password" name="password" value={this.state.password}
-                           onChange={this.handleChange}/>
-                </div>
-                <a href="https://www.google.nl" target="_blank">forgot password?</a>
-                <br/>
-                <button className="LoginButton" onClick={this.loginClicked}>Login</button>
+    return (
+        <div>
+            <h2 className="Login-H2">LOGIN</h2>
+            {this.state.hasLoginFailed ? <div>invalid credentials</div>: <div>login succesfull </div>}
+            <div><input placeholder="USERNAME" type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+            <br />
+            <input placeholder="PASSWORD" type="password" name="password" value={this.state.password} onChange={this.handleChange} />
             </div>
-        )
-    }
+            <a href="https://locolhost:8080/" target="_blank">forgot password?</a>
+            <br/>
+            <button onClick={this.loginClicked}>Login</button>
+        </div>
+   )}
 }
 
 export default Login;
