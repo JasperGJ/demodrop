e8eeab45478fd86985ce7b7b08c10f4e3a91b314
Robbie Keeris <robbie.keeris@gmail.com>
2019-12-12 20:22:39 +0100

    login is changed to loginform

--- src/reactjs/src/LogInRegister/LogInRegister.js
+++ src/reactjs/src/LogInRegister/LogInRegister.js
@@ -5,7 +5,7 @@ import {Link } from 'react-router-dom';
 const logInRegister = () => {
     return (
         <div className="ButtonsContainer">
-            <button  className="ButtonStart"><Link to="/login" className="Link">LOGIN</Link></button>
+            <button  className="ButtonStart"><Link to="/loginform" className="Link">LOGIN</Link></button>
             <br/>
             <button className="ButtonStart"><Link to="/signup" className="Link">SIGN UP</Link></button>
         </div>
