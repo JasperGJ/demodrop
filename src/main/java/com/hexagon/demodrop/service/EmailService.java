9e0ce88b14124205e2e70e8735fc933a6751a40e
Robbie Keeris <robbie.keeris@gmail.com>
2019-12-18 10:38:41 +0100

    Added configuration for Admin emailaccount and SMPTP

--- src/main/java/com/hexagon/demodrop/service/EmailService.java
+++ src/main/java/com/hexagon/demodrop/service/EmailService.java
@@ -1,6 +1,6 @@
 package com.hexagon.demodrop.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,16 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
     public JavaMailSender emailSender;
-/*  application  propperties
-    spring.mail.host=mail.robbiekeeris.nl
-    spring.mail.port=587
-    spring.mail.username=<hexagon@robbiekeeris.nl>
-    spring.mail.password=<hexagon>
-    spring.mail.properties.mail.smtp.auth=false
-    spring.mail.properties.mail.smtp.starttls.enable=true
-*/
+    private boolean enableSMTP;
+
+    public EmailService(@Value("${cust.enableSMTP}") boolean enableSMTP,JavaMailSender emailSender) {
+        this.enableSMTP = enableSMTP;
+        this.emailSender = emailSender;
+    }
 
     public void sendSimpleMessage(
             String to, String subject, String text) {
@@ -30,8 +27,16 @@ public class EmailService {
     }
 
     void sendMail(String title, String text, String email){
-        System.out.println(String.format("Mail to : %s\n subject: %s\n %s\n---------------",email,title,text));
-        //sendSimpleMessage(email,title,text);
+        System.out.println(String.format(
+                        "\n--------------------------------" +
+                        "\n--------------------------------" +
+                        "\nMail to : %s" +
+                        "\n subject: %s" +
+                        "\n--------------------------------" +
+                        "\n %s" +
+                        "\n--------------------------------",email,title,text));
+        if (enableSMTP)
+            sendSimpleMessage(email,title,text);
     }
 
 }
