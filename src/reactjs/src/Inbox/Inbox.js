a95e001b99a7a9501d78dad472860433cd1c4e5b
jks77 <jankrijn@gmail.com>
2019-12-18 12:28:51 +0100

    Inbox style aangepast

--- src/reactjs/src/Inbox/Inbox.js
+++ src/reactjs/src/Inbox/Inbox.js
@@ -1,22 +1,22 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './Inbox.css';
 
 const Inbox = () => {
-    const [hightlightedItem,setHightlight] = useState(-1);
+    const [hightlightedItem, setHightlight] = useState(-1);
 
-    const [selectedDemo,setSelectedDemo] = useState(-1);
-    const [comment,setComments] = useState("");
-    const [status,setStatus] = useState("");
+    const [selectedDemo, setSelectedDemo] = useState(-1);
+    const [comment, setComments] = useState("");
+    const [status, setStatus] = useState("");
 
-    const [templates,setTemplates] = useState([]);
-    const [opts,setOpts] = useState([]);
-    const [inbox,setInbox] = useState({
-        loaded : false,
-        data : {}
+    const [templates, setTemplates] = useState([]);
+    const [opts, setOpts] = useState([]);
+    const [inbox, setInbox] = useState({
+        loaded: false,
+        data: {}
     });
-    const [demo,setDemo] = useState({
-        loaded : false,
-        demo : {}
+    const [demo, setDemo] = useState({
+        loaded: false,
+        demo: {}
     });
 
 
@@ -24,7 +24,7 @@ const Inbox = () => {
         () => {
             getInbox();
             gettemplates();
-            }, [],
+        }, [],
     );
 
     const handleChange = (event) => {
@@ -32,29 +32,29 @@ const Inbox = () => {
     };
 
     const select = (event) => {
-         setComments(event.target.value);
+        setComments(event.target.value);
     };
 
-    const accept = () =>{
+    const accept = () => {
         setStatus("ACCEPT");
         const optsArray =
             templates.filter(template => template.status === "ACCEPT");
-        setOpts(optsArray.map((template,index) => <option key={index}>{template.comment}</option>));
+        setOpts(optsArray.map((template, index) => <option key={index}>{template.comment}</option>));
         setComments(optsArray[0].comment);
     };
 
-    const decline = () =>{
+    const decline = () => {
         setStatus("DECLINE");
         const optsArray =
             templates.filter(template => template.status === "DECLINE");
-        setOpts(optsArray.map((template,index) => <option key={index}>{template.comment}</option>));
+        setOpts(optsArray.map((template, index) => <option key={index}>{template.comment}</option>));
         setComments(optsArray[0].comment);
     };
 
     const start = () => {
-        fetch("/review/"+selectedDemo)
+        fetch("/review/" + selectedDemo)
             .then(responce => {
-                console.log("responce",responce);
+                console.log("responce", responce);
                 if (responce.status === 200) alert("De demo is set to review");
             })
             .catch(e => console.warn(e));
@@ -63,42 +63,42 @@ const Inbox = () => {
     const getInbox = () => {
         fetch("/inbox")
             .then(responce => {
-                console.log("responce",responce);
-                if (responce.status === 200)return responce.json();
+                console.log("responce", responce);
+                if (responce.status === 200) return responce.json();
             })
-            .then(json => setInbox({loaded : true, data : json}))
+            .then(json => setInbox({loaded: true, data: json}))
             .catch(e => console.warn(e));
     };
 
     const gettemplates = () => {
         fetch("/templates")
             .then(responce => {
-                if (responce.status === 200)return responce.json();
+                if (responce.status === 200) return responce.json();
             })
-            .then(json => setTemplates( [...json]))
+            .then(json => setTemplates([...json]))
             .catch(e => console.warn(e));
     };
 
     const getDemo = (id) => {
-        fetch("/demo/"+id)
+        fetch("/demo/" + id)
             .then(responce => {
-                console.log("responce",responce);
-                if (responce.status === 200)return responce.json();
+                console.log("responce", responce);
+                if (responce.status === 200) return responce.json();
             })
             .then(json => {
                 const blob = new Blob([json.audio], {type: 'audio/mp3'});
-                console.log("audioblob",blob);
-                setDemo({loaded : true, demo : json, audio : blob})
+                console.log("audioblob", blob);
+                setDemo({loaded: true, demo: json, audio: blob})
             })
             .catch(e => console.warn(e));
     };
 
-    const setComment = () =>{
+    const setComment = () => {
         const data = new FormData();
         data.append("status", status);
         data.append("comment", comment);
 
-        fetch("/review/"+selectedDemo, {
+        fetch("/review/" + selectedDemo, {
             method: "POST",
             body: data
         }).then(response => {
@@ -120,56 +120,66 @@ const Inbox = () => {
     };
 
     return (
+
+
         // css van ComponentContainerInbox staat in App.css
         <div className="ComponentContainerInbox">
             <div className="InboxContainer">
                 <div className="BoxDemos">
                     <h2 className="InboxH2">INBOX DEMOS</h2>
-                    {inbox.loaded
-                    ?
-                        inbox.data.items.map((item)  =>
-                            <div key={item.id}
-                                 onMouseLeave={() => highlightItem(-1)}
-                                 onMouseOver={() => highlightItem(item.id)}
-                                 onClick={() => itemClicked(item.id)}
-                                 className={
-                                     selectedDemo === item.id
-                                         ? "Demo Selected"
-                                         : hightlightedItem === item.id
-                                           ? "Demo Highlight"
-                                           : "Demo"
-                                 }>
+                    <div className="DemoInfo">
+                        {inbox.loaded
+                            ?
+                            inbox.data.items.map((item) =>
+                                <div key={item.id}
+                                     onMouseLeave={() => highlightItem(-1)}
+                                     onMouseOver={() => highlightItem(item.id)}
+                                     onClick={() => itemClicked(item.id)}
+                                     className={
+                                         selectedDemo === item.id
+                                             ? "Demo Selected"
+                                             : hightlightedItem === item.id
+                                             ? "Demo Highlight"
+                                             : "Demo"
+                                     }>
 
-                                <div className="Artist">{item.artist}</div>
-                                <div className="Status">{item.status}</div>
-                                <div className="Thumbnail"><img className="HexagonThumbnail" src={`data:image/png;base64,${item.thumbnail}`}
-                                                                alt="profile "/></div>
-                                <div className="Date">{item.uploaded}</div>
-                                {/*//todo de radio button moet nog gemaakt worden. probleem is dat als ik hem aanmaak, hij telkens centreert*/}
-                                <div className="TitleSong">{item.title}</div>
-                            </div>
-                        )
-                    :
-                        <div>ophalen</div>
-                    }
-
+                                    <div className="PhotoArtistDate">
+                                        <div className="Thumbnail"><img className="HexagonThumbnail"
+                                                                        src={`data:image/png;base64,${item.thumbnail}`}
+                                                                        alt="profile "/></div>
+                                        <div className="Artist">Artist: {item.artist}</div>
+                                        <div className="Date">{new Date(item.uploaded).toLocaleDateString()}</div>
+                                    </div>
+                                    <div className="StatusTitleSong">
+                                        <div className="Status">Status: {item.status}</div>
+                                        {/*//todo de radio button moet nog gemaakt worden. probleem is dat als ik hem aanmaak, hij telkens centreert*/}
+                                        <div className="TitleSong">Title: {item.title}</div>
+                                    </div>
+                                </div>
+                            )
+                            :
+                            <div>ophalen</div>
+                        }
+                    </div>
                 </div>
 
                 <div className="DemoShow">
                     <h2 className="InboxH2">DEMO</h2>
                     <div className="DemoInfo">
                         <h5>{demo.loaded
-                            ? demo.demo["artist"]+" - "+demo.demo.title
+                            ? demo.demo["artist"] + " - " + demo.demo.title
                             : ""}
                         </h5>
                         <audio src={demo.loaded ? `data:audio/mp3;base64,${demo.demo.audio}` : ""} controls/>
                         <br/>
+                        <button className="StartButton" onClick={start}>Start Review</button>
+                        <br/>
                         <button className="AcceptButton" onClick={accept}>Accept</button>
-                        <button className="StartButton" onClick={start}>Review</button>
                         <button className="DeclineButton" onClick={decline}>Decline</button>
                         <div className="DescriptionContainer">
                             <span className="Description">Description</span>
-                            <textarea  value={demo.loaded ? demo.demo.description : "niks"} className="TextAreaDescription" />
+                            <textarea value={demo.loaded ? demo.demo.description : "niks"}
+                                      className="TextAreaDescription"/>
                         </div>
                         <div className="AnswerContainer">
                             <div id="AnswerAndSelectMessageContainer">
